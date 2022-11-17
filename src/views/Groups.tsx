a23f395b7f6d8c5b4eb9a20592c6fa4abcadcf90
Matheus Michalski Puel <matheuspuel@gmail.com>
2022-11-16 21:49:29 -0300

    delete group

--- src/views/Groups.tsx
+++ src/views/Groups.tsx
@@ -19,12 +19,13 @@ import { getGroupById, getGroups, groupsSlice } from 'src/redux/slices/groups'
 import { useAppDispatch, useAppSelector } from 'src/redux/store'
 import { RootStackScreenProps } from 'src/routes/RootStack'
 import { Id } from 'src/utils/Entity'
-import { Eq, O, pipe } from 'src/utils/fp-ts'
+import { constVoid, Eq, O, pipe } from 'src/utils/fp-ts'
 
 export const Groups = (props: RootStackScreenProps<'Groups'>) => {
   const { navigation } = props
   const groups = useAppSelector(getGroups)
   const [modal, setModal] = useState<Option<Option<{ id: Id }>>>(none)
+  const [deleteModal, setDeleteModal] = useState<Option<{ id: Id }>>(none)
 
   useLayoutEffect(() => {
     navigation.setOptions({
@@ -52,11 +53,56 @@ export const Groups = (props: RootStackScreenProps<'Groups'>) => {
             data={item}
             parentProps={props}
             openEdit={id => setModal(some(some({ id })))}
+            openDelete={id => setDeleteModal(some({ id }))}
           />
         )}
       />
       <GroupModal {...props} state={modal} onClose={() => setModal(none)} />
+      <DeleteGroupModal
+        {...props}
+        state={deleteModal}
+        onClose={() => setDeleteModal(none)}
+      />
     </Flex>
+  )
+}
+const Item = (props: {
+  data: Group
+  parentProps: RootStackScreenProps<'Groups'>
+  openEdit: (id: Id) => void
+  openDelete: (id: Id) => void
+}) => {
+  const { name, id } = props.data
+  const { navigation } = props.parentProps
+
+  return (
+    <Pressable onPress={() => navigation.navigate('Group', { id })}>
+      <Flex bg="white" direction="row" m="2" p="2" rounded="lg" shadow="1">
+        <Text flex={1} isTruncated bold>
+          {name}
+        </Text>
+        <Pressable
+          px="1"
+          onPress={() => {
+            props.openEdit(id)
+          }}
+        >
+          <Icon size="lg" color="gray.500" as={<MaterialIcons name="edit" />} />
+        </Pressable>
+        <Pressable
+          px="1"
+          onPress={() => {
+            props.openDelete(id)
+          }}
+        >
+          <Icon
+            size="lg"
+            color="gray.500"
+            as={<MaterialIcons name="delete" />}
+          />
+        </Pressable>
+      </Flex>
+    </Pressable>
   )
 }
 
@@ -139,41 +185,72 @@ const GroupModal = (
   )
 }
 
-const Item = (props: {
-  data: Group
-  parentProps: RootStackScreenProps<'Groups'>
-  openEdit: (id: Id) => void
-}) => {
-  const { name, id } = props.data
-  const { navigation } = props.parentProps
+const DeleteGroupModal = (
+  props: RootStackScreenProps<'Groups'> & {
+    state: Option<{ id: Id }>
+    onClose: () => void
+  },
+) => {
+  const group = useAppSelector(
+    pipe(
+      props.state,
+      O.map(({ id }) => id),
+      O.map(getGroupById),
+      O.getOrElseW(() => () => none),
+    ),
+    O.getEq(Eq.eqStrict).equals,
+  )
+  const dispatch = useAppDispatch()
 
   return (
-    <Pressable onPress={() => navigation.navigate('Group', { id })}>
-      <Flex bg="white" direction="row" m="2" p="2" rounded="lg" shadow="1">
-        <Text flex={1} isTruncated bold>
-          {name}
-        </Text>
-        <Pressable
-          px="1"
-          onPress={() => {
-            props.openEdit(id)
-          }}
-        >
-          <Icon size="lg" color="gray.500" as={<MaterialIcons name="edit" />} />
-        </Pressable>
-        <Pressable
-          px="1"
-          onPress={() => {
-            //
-          }}
-        >
-          <Icon
-            size="lg"
-            color="gray.500"
-            as={<MaterialIcons name="delete" />}
-          />
-        </Pressable>
-      </Flex>
-    </Pressable>
+    <Modal isOpen={O.isSome(props.state)}>
+      <Modal.Content>
+        <Modal.Header>
+          Excluir grupo
+          <Modal.CloseButton onPress={props.onClose} />
+        </Modal.Header>
+        <Modal.Body>
+          {pipe(
+            group,
+            O.matchW(
+              () => null,
+              g => (
+                <Text>
+                  Deseja excluir o grupo {<Text bold>{g.name}</Text>} e todos os
+                  jogadores?
+                </Text>
+              ),
+            ),
+          )}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button.Group space="2">
+            <Button
+              variant="ghost"
+              colorScheme="danger"
+              onPress={props.onClose}
+            >
+              Cancelar
+            </Button>
+            <Button
+              colorScheme="danger"
+              onPress={() => {
+                props.onClose()
+                pipe(
+                  group,
+                  O.match(
+                    () => constVoid,
+                    g => () =>
+                      dispatch(groupsSlice.actions.delete({ id: g.id })),
+                  ),
+                )()
+              }}
+            >
+              Excluir
+            </Button>
+          </Button.Group>
+        </Modal.Footer>
+      </Modal.Content>
+    </Modal>
   )
 }
