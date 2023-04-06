435e7c1627dd4b1405ba6ed5a226159d0e3a2a22
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-04-06 16:19:55 -0300

    position player count test

--- src/datatypes/TeamsGenerator.test.ts
+++ src/datatypes/TeamsGenerator.test.ts
@@ -1,9 +1,9 @@
 /* eslint-disable functional/no-expression-statements */
 import * as Arb from '@effect/schema/Arbitrary'
 import * as fc from 'fast-check'
-import { $, A, Monoid, Num } from 'fp'
+import { $, A, Monoid, Num, Rec } from 'fp'
 import { playersMock } from 'src/mocks/Player'
-import { Player } from '.'
+import { Player, Position } from '.'
 import { balanceTeamsByCriteria } from './TeamsGenerator'
 
 describe('Balance teams', () => {
@@ -49,6 +49,32 @@ describe('Balance teams', () => {
             teams.every(a =>
               teams.every(b => Math.abs(a.length - b.length) <= 1),
             ),
+          ),
+      ),
+    )
+  })
+
+  it('should not have teams with player count difference higher than one in any position', () => {
+    fc.assert(
+      fc.property(
+        fc.integer({ min: 1, max: 20 }),
+        fc.boolean(),
+        fc.array(Arb.to(Player.Schema)(fc)),
+        (n, rating, players) =>
+          $(
+            balanceTeamsByCriteria({ position: true, rating })(n)(players),
+            teams =>
+              teams.every(a =>
+                teams.every(b =>
+                  Rec.keys(Position.Dict).every(
+                    pos =>
+                      Math.abs(
+                        a.filter(p => p.position === pos).length -
+                          b.filter(p => p.position === pos).length,
+                      ) <= 1,
+                  ),
+                ),
+              ),
           ),
       ),
     )
