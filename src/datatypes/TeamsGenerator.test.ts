10cf59f8d485c16e942f6b6bd514baf6db07b8f4
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-04-10 19:41:26 -0300

    test result same players

--- src/datatypes/TeamsGenerator.test.ts
+++ src/datatypes/TeamsGenerator.test.ts
@@ -1,7 +1,7 @@
 /* eslint-disable functional/no-expression-statements */
 import * as Arb from '@effect/schema/Arbitrary'
 import * as fc from 'fast-check'
-import { $, A, Monoid, NEA, Num, O, Rec, SG, constant } from 'fp'
+import { $, A, Eq, NEA, O, Rec, SG, constant } from 'fp'
 import { playersMock } from 'src/mocks/Player'
 import { Id } from 'src/utils/Entity'
 import { Player, Position } from '.'
@@ -44,47 +44,53 @@ const balanceTeamsByFitOrdTestDouble: typeof balanceTeamsByFitOrd =
       O.getOrElseW(constant([])),
     )
 
-const playersCounterExample1: Array<Player> = [
-  { active: false, id: Id(''), name: '', rating: 6.5, position: 'M' },
-  { active: false, id: Id(''), name: '', rating: 3.5, position: 'M' },
-  { active: false, id: Id(''), name: '', rating: 2.5, position: 'LE' },
-  { active: false, id: Id(''), name: '', rating: 3, position: 'M' },
-  { active: false, id: Id(''), name: '', rating: 5, position: 'Z' },
-]
+const balanceTeamsArb = fc.record({
+  n: fc.integer({ min: 2, max: 9 }),
+  params: fc.record({
+    position: fc.boolean(),
+    rating: fc.boolean(),
+  }),
+  players: fc.array(Arb.to(Player.Schema)(fc)),
+})
 
 describe('Balance teams', () => {
   it('should return the optimal solution', () => {
+    const playersCounterExample1: Array<Player> = [
+      { active: false, id: Id(''), name: '', rating: 6.5, position: 'M' },
+      { active: false, id: Id(''), name: '', rating: 3.5, position: 'M' },
+      { active: false, id: Id(''), name: '', rating: 2.5, position: 'LE' },
+      { active: false, id: Id(''), name: '', rating: 3, position: 'M' },
+      { active: false, id: Id(''), name: '', rating: 5, position: 'Z' },
+    ]
     fc.assert(
       fc.property(
-        fc.integer({ min: 1, max: 3 }),
-        fc.boolean(),
-        fc.boolean(),
+        fc.integer({ min: 2, max: 4 }),
+        fc.record({ position: fc.boolean(), rating: fc.boolean() }),
         fc.array(Arb.to(Player.Schema)(fc), { minLength: 1, maxLength: 6 }),
-        (n, position, rating, players) =>
-          $(getFitOrdFromCriteria({ position, rating }), fitOrd =>
+        (n, params, players) =>
+          $(getFitOrdFromCriteria(params), fitOrd =>
             fitOrd.equals(
               balanceTeamsByFitOrd(fitOrd)(n)(players),
               balanceTeamsByFitOrdTestDouble(fitOrd)(n)(players),
             ),
           ),
       ),
-      { examples: [[2, true, true, playersCounterExample1]] },
+      {
+        examples: [
+          [2, { position: true, rating: true }, playersCounterExample1],
+        ],
+      },
     )
   })
 
-  it('should return the same number of players', () => {
+  it('should return the same players', () => {
     fc.assert(
-      fc.property(
-        fc.integer({ min: 1, max: 20 }),
-        fc.boolean(),
-        fc.boolean(),
-        fc.array(Arb.to(Player.Schema)(fc)),
-        (n, position, rating, players) =>
-          Monoid.concatAll(Num.MonoidSum)(
-            balanceTeamsByCriteria({ position, rating })(n)(players).map(
-              A.size,
-            ),
-          ) === players.length,
+      fc.property(balanceTeamsArb, ({ n, params, players }) =>
+        $(
+          balanceTeamsByCriteria(params)(n)(players),
+          A.flatten,
+          Eq.equals(A.getUnorderedEq(Eq.eqStrict))(players),
+        ),
       ),
     )
   })
@@ -92,55 +98,43 @@ describe('Balance teams', () => {
   it('should return the correct number of teams', () => {
     fc.assert(
       fc.property(
-        fc.integer({ min: 1, max: 20 }),
-        fc.boolean(),
-        fc.boolean(),
-        fc.array(Arb.to(Player.Schema)(fc)),
-        (n, position, rating, players) =>
-          balanceTeamsByCriteria({ position, rating })(n)(players).length === n,
+        balanceTeamsArb,
+        ({ n, params, players }) =>
+          balanceTeamsByCriteria(params)(n)(players).length === n,
       ),
     )
   })
 
   it('should not have teams with player count difference higher than one', () => {
     fc.assert(
-      fc.property(
-        fc.integer({ min: 1, max: 20 }),
-        fc.boolean(),
-        fc.boolean(),
-        fc.array(Arb.to(Player.Schema)(fc)),
-        (n, position, rating, players) =>
-          $(balanceTeamsByCriteria({ position, rating })(n)(players), teams =>
-            teams.every(a =>
-              teams.every(b => Math.abs(a.length - b.length) <= 1),
-            ),
+      fc.property(balanceTeamsArb, ({ n, params, players }) =>
+        $(balanceTeamsByCriteria(params)(n)(players), teams =>
+          teams.every(a =>
+            teams.every(b => Math.abs(a.length - b.length) <= 1),
           ),
+        ),
       ),
     )
   })
 
   it('should not have teams with player count difference higher than one in any position', () => {
     fc.assert(
-      fc.property(
-        fc.integer({ min: 1, max: 20 }),
-        fc.boolean(),
-        fc.array(Arb.to(Player.Schema)(fc)),
-        (n, rating, players) =>
-          $(
-            balanceTeamsByCriteria({ position: true, rating })(n)(players),
-            teams =>
-              teams.every(a =>
-                teams.every(b =>
-                  Rec.keys(Position.Dict).every(
-                    pos =>
-                      Math.abs(
-                        a.filter(p => p.position === pos).length -
-                          b.filter(p => p.position === pos).length,
-                      ) <= 1,
-                  ),
+      fc.property(balanceTeamsArb, ({ n, params, players }) =>
+        $(
+          balanceTeamsByCriteria({ ...params, position: true })(n)(players),
+          teams =>
+            teams.every(a =>
+              teams.every(b =>
+                Rec.keys(Position.Dict).every(
+                  pos =>
+                    Math.abs(
+                      a.filter(p => p.position === pos).length -
+                        b.filter(p => p.position === pos).length,
+                    ) <= 1,
                 ),
               ),
-          ),
+            ),
+        ),
       ),
     )
   })
--- src/utils/fp/Array.ts
+++ src/utils/fp/Array.ts
@@ -1,4 +1,25 @@
+import * as A from 'fp-ts/Array'
+import * as O from 'fp-ts/Option'
+import { pipe as $ } from 'fp-ts/function'
+import * as Eq from './Eq'
+
 export * from 'fp-ts/Array'
 
 export const isArray = <A>(a: A): a is Extract<A, ReadonlyArray<unknown>> =>
   Array.isArray(a)
+
+export const getUnorderedEq = <A>(E: Eq.Eq<A>): Eq.Eq<Array<A>> =>
+  Eq.fromEquals((as, bs) =>
+    $(
+      as,
+      A.matchLeft(
+        () => A.isEmpty(bs),
+        (a, as_) =>
+          $(
+            A.findIndex(Eq.equals(E)(a))(bs),
+            O.chain(i => A.deleteAt(i)(bs)),
+            O.matchW(() => false, Eq.equals(getUnorderedEq(E))(as_)),
+          ),
+      ),
+    ),
+  )
