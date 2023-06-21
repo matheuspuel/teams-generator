df706973de8a3ed1bde4545422ead29f60210c53
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-06-21 19:31:13 -0300

    versionCode 11

--- app.config.ts
+++ app.config.ts
@@ -51,7 +51,7 @@ const getConfig = (): ExpoConfig => ({
         preview: '.preview',
         development: '.development',
       }),
-    versionCode: 10,
+    versionCode: 11,
     adaptiveIcon: {
       backgroundColor: '#136d15',
       foregroundImage:
