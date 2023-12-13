9dc8ae33bf85b486233e934231d096f768c39dac
Matheus Michalski Puel <matheuspuel@gmail.com>
2023-12-13 19:56:22 -0300

    ios borderless button dimmed instead of ripple

--- src/components/react-native/Pressable.ts
+++ src/components/react-native/Pressable.ts
@@ -1,13 +1,12 @@
-import { Runtime, pipe } from 'fp'
+import { Runtime, identity, pipe } from 'fp'
 import React from 'react'
-import { RectButton } from 'react-native-gesture-handler'
+import { BorderlessButton, RectButton } from 'react-native-gesture-handler'
 import {
   AbsolutePositionProps,
   Children,
   FlexChildProps,
   FlexContainerProps,
   GapProps,
-  JSXElementsChildren,
   MarginProps,
   PaddingProps,
   UIColor,
@@ -16,7 +15,6 @@ import {
 import { useRuntime } from 'src/contexts/Runtime'
 import { useThemeGetRawColor } from 'src/contexts/Theme'
 import { AppEvent } from 'src/events'
-import { AppRuntime } from 'src/runtime'
 import { Colors } from 'src/services/Theme'
 import { named2 } from '../hyperscript'
 
@@ -47,106 +45,91 @@ export type PressableProps = PressableStyleProps & {
   foreground?: boolean
 }
 
-export type PressableArgs = {
-  x: PressableProps
-  children?: JSXElementsChildren
-  runtime: AppRuntime
-  getRawColor: (color: UIColor) => string
-}
-
-const getRawProps = ({
-  x: props,
-  children,
-  runtime,
-  getRawColor,
-}: PressableArgs): React.ComponentProps<typeof RectButton> => ({
-  children: children,
-  onPress:
-    props.isEnabled !== false
-      ? () => Runtime.runPromise(runtime)(props.onPress)
-      : undefined,
-  rippleColor:
-    props.isEnabled !== false
-      ? props.rippleColor
-        ? getRawColor(
-            Colors.opacity(props.rippleOpacity ?? 1)(props.rippleColor),
-          )
-        : undefined
-      : 'transparent',
-  activeOpacity: props.isEnabled !== false ? props.rippleOpacity : 0,
-  underlayColor: props.rippleColor ? getRawColor(props.rippleColor) : undefined,
-  borderless: props.borderless,
-  foreground: props.foreground,
-  style: {
-    padding: props?.p,
-    paddingHorizontal: props?.px,
-    paddingVertical: props?.py,
-    paddingLeft: props?.pl,
-    paddingRight: props?.pr,
-    paddingTop: props?.pt,
-    paddingBottom: props?.pb,
-    margin: props?.m,
-    marginHorizontal: props?.mx,
-    marginVertical: props?.my,
-    marginLeft: props?.ml,
-    marginRight: props?.mr,
-    marginTop: props?.mt,
-    marginBottom: props?.mb,
-    borderRadius: props?.round,
-    gap: props?.gap,
-    rowGap: props?.gapX,
-    columnGap: props?.gapY,
-    width: props?.w,
-    height: props?.h,
-    minWidth: props?.minW,
-    minHeight: props?.minH,
-    aspectRatio: props?.aspectRatio,
-    flex: props?.flex,
-    flexDirection: props?.direction,
-    backgroundColor: props?.bg ? getRawColor(props.bg) : undefined,
-    borderColor: props?.borderColor
-      ? getRawColor(props.borderColor)
-      : undefined,
-    justifyContent:
-      props?.justify === 'start'
-        ? 'flex-start'
-        : props?.justify === 'end'
-          ? 'flex-end'
-          : props?.justify,
-    alignItems:
-      props?.align === 'start'
-        ? 'flex-start'
-        : props?.align === 'end'
-          ? 'flex-end'
-          : props?.align,
-    alignSelf:
-      props?.alignSelf === 'start'
-        ? 'flex-start'
-        : props?.alignSelf === 'end'
-          ? 'flex-end'
-          : props?.alignSelf,
-    elevation: props?.shadow,
-    ...pipe(
-      props?.absolute
-        ? { ...props.absolute, position: 'absolute' }
-        : props?.absolute === false
-          ? { position: 'relative' }
-          : undefined,
-    ),
-  },
-})
-
-const Pressable_ = (args: PressableArgs) =>
-  React.createElement(RectButton, getRawProps(args))
-
 export const Pressable = named2('Pressable')((props: PressableProps) =>
   // eslint-disable-next-line react/display-name
   (children: Children): UIElement => {
     const runtime = useRuntime()
     const getRawColor = useThemeGetRawColor()
     return React.createElement(
-      Pressable_,
-      { x: props, runtime, getRawColor },
+      props.borderless ? BorderlessButton : RectButton,
+      identity<React.ComponentProps<typeof RectButton>>({
+        children: children,
+        onPress:
+          props.isEnabled !== false
+            ? () => Runtime.runPromise(runtime)(props.onPress)
+            : undefined,
+        rippleColor:
+          props.isEnabled !== false
+            ? props.rippleColor
+              ? getRawColor(
+                  Colors.opacity(props.rippleOpacity ?? 1)(props.rippleColor),
+                )
+              : undefined
+            : 'transparent',
+        activeOpacity: props.isEnabled !== false ? props.rippleOpacity : 0,
+        underlayColor: props.rippleColor
+          ? getRawColor(props.rippleColor)
+          : undefined,
+        borderless: props.borderless,
+        foreground: props.foreground,
+        style: {
+          padding: props?.p,
+          paddingHorizontal: props?.px,
+          paddingVertical: props?.py,
+          paddingLeft: props?.pl,
+          paddingRight: props?.pr,
+          paddingTop: props?.pt,
+          paddingBottom: props?.pb,
+          margin: props?.m,
+          marginHorizontal: props?.mx,
+          marginVertical: props?.my,
+          marginLeft: props?.ml,
+          marginRight: props?.mr,
+          marginTop: props?.mt,
+          marginBottom: props?.mb,
+          borderRadius: props?.round,
+          gap: props?.gap,
+          rowGap: props?.gapX,
+          columnGap: props?.gapY,
+          width: props?.w,
+          height: props?.h,
+          minWidth: props?.minW,
+          minHeight: props?.minH,
+          aspectRatio: props?.aspectRatio,
+          flex: props?.flex,
+          flexDirection: props?.direction,
+          backgroundColor: props?.bg ? getRawColor(props.bg) : undefined,
+          borderColor: props?.borderColor
+            ? getRawColor(props.borderColor)
+            : undefined,
+          justifyContent:
+            props?.justify === 'start'
+              ? 'flex-start'
+              : props?.justify === 'end'
+                ? 'flex-end'
+                : props?.justify,
+          alignItems:
+            props?.align === 'start'
+              ? 'flex-start'
+              : props?.align === 'end'
+                ? 'flex-end'
+                : props?.align,
+          alignSelf:
+            props?.alignSelf === 'start'
+              ? 'flex-start'
+              : props?.alignSelf === 'end'
+                ? 'flex-end'
+                : props?.alignSelf,
+          elevation: props?.shadow,
+          ...pipe(
+            props?.absolute
+              ? { ...props.absolute, position: 'absolute' }
+              : props?.absolute === false
+                ? { position: 'relative' }
+                : undefined,
+          ),
+        },
+      }),
       ...children,
     )
   },
